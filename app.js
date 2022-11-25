2c48c93fc3a89891bb15d602675ca3c1f44d0c9b
devahtesham <ahte02254@gmail.com>
2022-11-25 11:31:39 +0500

    variables and primitive

---
+++ app.js
@@ -0,0 +1,49 @@
+/*
+ES5 >> var
+ES6 >> let const 
+*/
+/*
+ STEPS TO CREATE VARIABLES
+ 1) declaration
+ 2) initialization
+*/ 
+//================= var 
+// var myName; // variable declaration
+// myName = "ahtesham"  // initialization
+// var myName = "ahtesham"
+// myName = "anus"
+//================= let
+// let myName = "ahtesham"
+//  myName = "anus"
+//================= const
+// console.log("name",myName);
+// var myName = "ahtesham"
+
+
+
+
+
+
+
+
+
+/*
+Primitive type:
+    string
+    number
+    boolean
+    null
+    undefined
+Refrence type
+    array
+    object
+    functions
+*/
+
+// string
+// let course = "java script";
+// let newWord = course.trim()
+// console.log(newWord.length);
+// let ans = newWord.length/5;
+// console.log(ans);
+

372b9ae68f0e1199740938337670274c543efdcd
devahtesham <ahte02254@gmail.com>
2022-11-26 12:22:16 +0500

    array methods

--- app.js
+++ app.js
@@ -47,3 +47,28 @@ Refrence type
 // let ans = newWord.length/5;
 // console.log(ans);
 
+// let arr=[22,15,13,26,8,7,1];
+// let arr1=[9,6,1,3,2,0];
+let arr=[10,20,30,40,50,60,70];
+// let arr=["canada","bashir","denim","ear","asif","monday","tuesday"];
+// console.log(arr);
+// arr.sort();
+// arr.reverse();
+
+// let ans = arr.indexOf("bashir");
+// let ans = arr.lastIndexOf("bashir");
+// let ans = arr.includes("Asif")
+// let ans = arr.findIndex(function(num){
+//     return num==="js"
+// })
+// let ans = arr.concat(arr1)
+// let ans = arr1.concat(arr)
+// let ans = arr.slice(1,9)
+// console.log(arr);
+// let newArr = []
+// console.log(arr);
+let ans = arr.map(function(num){
+console.log(num*10); 
+})
+// console.log(ans);
+
